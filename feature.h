e5a35905ec94e860d5e48c466696399505636105
Edward Cantillo <ed.cali1960@gmail.com>
2024-10-14 22:32:48 -0400

    initial files for feature header file

---
+++ feature.h
@@ -0,0 +1,20 @@
+// this include file has the defines for the applications 
+
+#ifndef INC_FEATURES
+
+typedef enum 
+{
+    APP_FEATURE1,
+    APP_FEATURE2,
+    APP_FEATURE3,
+    APP_FEATURE4,
+    APP_FEATURE5,
+    APP_FEATURE6,
+    APP_FEATURE7,
+    APP_FEATURE8,
+
+} app_FEATRUES;
+
+
+
+#endif
